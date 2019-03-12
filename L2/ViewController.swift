68179a9891dfb3b53638f30b73d1169d4e7f7d2f
Chang Liu <bobbyliu117@gmail.com>
2019-03-11 21:19:06 -0700

    init

---
+++ L2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  L2
+//
+//  Created by Chang Liu on 3/11/19.
+//  Copyright © 2019 Chang Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
