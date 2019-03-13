417482f7317c4ae4e1adab176043fb0c3b109a01
Chang Liu <bobbyliu117@gmail.com>
2019-03-13 07:59:00 -0700

    1

---
+++ L2/controllers/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  L2
+//
+//  Created by Chang Liu on 3/12/19.
+//  Copyright © 2019 Chang Liu. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
